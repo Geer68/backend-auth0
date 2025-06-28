241a37e8b7c53ec310e253d7a41a15791239fe38
Geer68 <germhidalgo@gmail.com>
2025-06-28 20:17:45 -0300

    First Commit

---
+++ src/main/java/com/example/backend_auth0/BackendAuth0Application.java
@@ -0,0 +1,13 @@
+package com.example.backend_auth0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAuth0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAuth0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_auth0/BackendAuth0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_auth0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAuth0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
